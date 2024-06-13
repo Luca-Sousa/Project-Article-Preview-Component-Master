33db3bc39dc02294b50be65f529f46e1a0d841ef
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-06-13 16:25:10 -0300

    Update Jquery Project

--- src/script.js
+++ src/script.js
@@ -1,5 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    
+    $('#share').click(function(){
+        if($('#option-share').is(':visible')) {
+            $('#user').show();
+            $('#option-share').hide();
+        } else {
+            if ($(Window).width() < 1024){
+                $('#user').hide();
+            }
+            $('#option-share').show();
+        }
+    });
+
+    $('#back').click(function(){
+        $('#option-share').hide();
+        $('#user').show();
+    });
 
 });
