821f8bae1cfa8060b6db641b08d66a0468e2a2d9
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-06-13 16:41:44 -0300

    Project version 1.0 Completed

--- src/script.js
+++ src/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+    $(window).resize(function() {
+        $('#user').show();
+        $('#option-share').hide();
+    });
+
     $('#share').click(function(){
         if($('#option-share').is(':visible')) {
             $('#user').show();
